02f46401ee3997e5511711aa3d8b3915b038c4a4
atotared <atota@red.software.systems>
2021-09-16 23:07:32 +0200

    Install babel, eslint, prettier and a bunch of presets. Add config files for babel, eslint, prettier and TypeScript. Update .gitignore

--- src/components/routes/Root.tsx
+++ src/components/routes/Root.tsx
@@ -1,20 +1,14 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import { Routes } from "./urls";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Routes } from './urls';
 
 const Root = () => {
-    return (
-        <Router>
-            <Switch>
-            <Route path={Routes.HOME}>
-                Home
-            </Route>
-            </Switch>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Switch>
+        <Route path={Routes.HOME}>Home</Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Root;
